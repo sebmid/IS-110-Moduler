a321cdf0ae4781d78c62ab9e9559641c74e72d8e
sebmid <sebmid@live.com>
2022-01-14 17:18:34 +0100

    added files

---
+++ Modul-1/Kalle metoder som returnerer en verdi/Modul 1 blueJ prosjekt/Tall.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class Tall here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Tall
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    private int y;
+
+    /**
+     * Constructor for objects of class Tall
+     */
+    public Tall()
+    {
+        // initialise instance variables
+        x = 0;
+        y = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y, int x)
+    {
+        // put your code here
+        return x + y;
+    }
+}
---
+++ Modul-1/Kalle metoder som returnerer en verdi/Modul 1 blueJ prosjekt/Tall1.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class Tall1 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Tall1
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    private int y;
+    /**
+     * Constructor for objects of class Tall1
+     */
+    public Tall1()
+    {
+        // initialise instance variables
+        x = 0;
+        y = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y, int x)
+    {
+        // put your code here
+        return x + y;
+    }
+}
